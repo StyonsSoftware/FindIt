fcffd401d98f416e82117a96fb3468136170afdf
JosephStyons_cp <SND\JosephStyons_cp>
2014-09-14 05:15:57 +0000

    updated version

--- Findit/Findit/Properties/AssemblyInfo.cs
+++ Findit/Findit/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FoundIt")]
+[assembly: AssemblyTitle("FindIt")]
 [assembly: AssemblyDescription("A simple utility to search for text, with some useful options to help you find what you really need.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("jstyons@gmail.com")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
